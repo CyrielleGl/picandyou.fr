908724e7cf5bf824cd213c0766aff8f65e7645f7
CyrielleGl <cyrielle.gallou@gmail.com>
2020-07-11 16:36:07 +0200

    Contact form - functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { ContactService } from './contact.service';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HomeComponent } from './home/home.component';
 import { PlateformeComponent } from './plateforme/plateforme.component';
@@ -13,6 +17,7 @@ import { SoutienComponent } from './soutien/soutien.component';
 import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
+
   declarations: [
     AppComponent,
     HomeComponent,
@@ -22,12 +27,19 @@ import { ContactComponent } from './contact/contact.component';
     SoutienComponent,
     ContactComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+
+  providers: [
+    ContactService
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators, NgForm } from '@angular/forms';
+import { ContactService } from '../contact.service';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
+
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  FormData: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private builder: FormBuilder, private contact: ContactService) { }
+
+  ngOnInit() {
+    this.FormData = this.builder.group({
+    Prenom: new FormControl(''),
+    Nom: new FormControl(''),
+    Email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
+    Message: new FormControl('', [Validators.compose([Validators.required])]),
+    Check: new FormControl('')
+    });
   }
 
-}
+  onsubmit(FormData) {
+    console.log(FormData);
+    this.contact.PostMessage(FormData)
+      .subscribe(response => {
+        location.href = 'https://mailthis.to/confirm';
+        console.log(response);
+      },
+      error => {
+        console.warn(error.responseText);
+        console.log({ error });
+      });
+  }
+  }
