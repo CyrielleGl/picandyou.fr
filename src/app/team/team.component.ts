689c78ba9c481dee32c6d28e6f5a41adbc9763d4
CyrielleGl <cyrielle.gallou@gmail.com>
2020-07-16 21:08:37 +0200

    ngIf Team display worker

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-team',
@@ -14,9 +14,25 @@ export class TeamComponent implements OnInit {
       job: 'Développeuse web | Fondatrice de Pic&You',
       describe: `<b>Photographe passionnée et modèle amateur</b>, j\'ai enduré l\'utilisation de nombreux groupes Facebook pour trouver des collaborations, l\'organisation de celles-ci à travers mon profil personnel, le manque de possibilités pour organiser ce genre d\'activité, et le nombre de discussions perdues dans mes échanges personnels.\
       \nAprès une <b>reconversion professionnelle en programmation web</b>, il était donc évident pour moi de créer une plateforme communautaire telle que Pic&You.`,
-      photo: '../../assets/visuels/photos-team/cyrielle.png'
+      photo: '../../assets/visuels/photos-team/cyrielle.png',
+      display: true,
+    },
+    {
+      name: 'Toto',
+      job: 'Développeur web',
+      describe: ``,
+      photo: '../../assets/visuels/photos-team',
+      display: false,
+    },
+    {
+      name: 'Toto',
+      job: 'Développeur web',
+      describe: ``,
+      photo: '../../assets/visuels/photos-team',
+      display: false,
     }
   ];
+
 
   constructor() { }
 
--- src/app/team/worker/worker.component.ts
+++ src/app/team/worker/worker.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class WorkerComponent implements OnInit {
 
-@Input()
-worker: any;
+  @Input()
+  worker: any;
 
   constructor() { }
 
